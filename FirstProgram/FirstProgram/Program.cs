0649c1fb5d88d109e8c663fd54adf139ce2174cb
eLucas45 <90000780+eLucas45@users.noreply.github.com>
2021-09-09 10:54:16 +0200

    1. Commit

---
+++ FirstProgram/FirstProgram/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProgram
+{
+    class Program
+    {
+        
+
+        /// <summary>
+        /// Main methord; called on startup
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //Code here
+            Console.Title = "Hello World";
+            Console.WriteLine("Hello World");
+
+            //keep Program from closing
+            Console.ReadLine();
+        }
+
+        
+    }
+}
